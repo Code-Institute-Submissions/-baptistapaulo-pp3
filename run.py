df31bebb36e538837c0f1132ee3737e8958baaa2
baptistapaulo <paulo.a.baptista@gmail.com>
2022-11-18 22:53:17 +0000

    Add logo and welcome message

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import pyfiglet
+
+# Welcome Message
+logo = pyfiglet.figlet_format("IP Subnetting")
+print(logo)
+print("Welcome to the 'IP Subnet Calculator' tool.\n")
+print("This tool pretends to help negineers on their "
+      "daily support tasks or projects.\n")
+print("Engineers will be able to:\n")
+print("- calculate available IPs\n")
+print("- determine subnet range\n")
+print("- generate a random IP\n")
