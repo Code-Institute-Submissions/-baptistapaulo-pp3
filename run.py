5b5ef317b39872bc39ff9299597631e2e09872fd
baptistapaulo <paulo.a.baptista@gmail.com>
2022-11-19 08:59:10 +0000

    Removed main() function as it is was not working yet

--- run.py
+++ run.py
@@ -23,10 +23,6 @@ def dec2bin(integer):
 
 
 def complement(number):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     if number == '0':
         number = '1'
     elif number == '.':
@@ -37,10 +33,6 @@ def complement(number):
 
 
 def find_wildcard(binary_subnet):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     binary_list = list(binary_subnet)
     wildcard = ''.join(complement(binary_list[y])
                        for y in range(len(binary_list)))
@@ -48,10 +40,6 @@ def find_wildcard(binary_subnet):
 
 
 def convert_decimal(wildcard_Binary):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     binary = {}
     for x in range(4):
         binary[x] = int(wildcard_Binary.split(".")[x], 2)
@@ -62,10 +50,6 @@ def convert_decimal(wildcard_Binary):
 
 
 def andOP(IP1, IP2):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     ID_list = {}
     for y in range(4):
         ID_list[y] = int(IP1.split(".")[y]) & int(IP2.split(".")[y])
@@ -76,10 +60,6 @@ def andOP(IP1, IP2):
 
 
 def orOP(IP1, IP2):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     Broadcast_list = {}
     for z in range(4):
         Broadcast_list[z] = int(IP1.split(".")[z]) | int(IP2.split(".")[z])
@@ -90,10 +70,6 @@ def orOP(IP1, IP2):
 
 
 def maxiIP(broadcastIP):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     maxIPs = broadcastIP.split(".")
     if int(broadcastIP.split(".")[3]) - 1 == 0:
         if int(broadcastIP.split(".")[2]) - 1 == 0:
@@ -111,10 +87,6 @@ def maxiIP(broadcastIP):
 
 
 def miniIP(networkID):
-    """
-    This code is referenced in
-    https://github.com/EmreOvunc/Subnetting/blob/master/Subnetting.py
-    """
     miniIPs = networkID.split(".")
     if int(networkID.split(".")[3]) + 1 == 256:
         if int(networkID.split(".")[2]) + 1 == 256:
@@ -128,8 +100,8 @@ def miniIP(networkID):
                 miniIPs[2] = 0
                 miniIPs[3] = 0
         else:
-            (networkID.split("."))[2] = int(networkID.split(".")[2]) + 1
-            (networkID.split("."))[3] = 0
+            miniIPs[2] = int(networkID.split(".")[2]) + 1
+            miniIPs[3] = 0
     else:
         miniIPs[3] = int(networkID.split(".")[3]) + 1
     return ".".join(str(miniIPs[x]) for x in range(4))
@@ -144,32 +116,6 @@ def menu():
         "[2] Determine subnet range\n"
         "[3] Generate a random IP\n"
     )
-    if option == '1':
-        print("available IPs")
-    elif option == '2':
-        cls_terminal()
-        logo = pyfiglet.figlet_format("IP Subnetting")
-        print(logo)
-        ip_binary = dec2bin(ip)
-        subnet_binary = dec2bin(subnet)
-        print('\nIP:', ip)
-        print('Subnet:', subnet)
-        wildcard_binary = find_wildcard(dec2bin(subnet))
-        WildCard = convert_decimal(wildcard_binary)
-        networkID = andOP(ip, subnet)
-        network_Binary = dec2bin(networkID)
-        print('Network ID:', networkID)
-        broadcastIP = orOP(networkID, WildCard)
-        broadcastIP_binary = dec2bin(broadcastIP)
-        print('Broadcast IP:', broadcastIP)
-        maxIP = maxiIP(broadcastIP)
-        maxIP_binary = dec2bin(maxiIP)
-        print('Max. IP:', maxIP)
-        minIP = miniIP(networkID)
-        minIP_binary = dec2bin(networkID)
-        print('Min. IP:', minIP)
-    else:
-        print("random IP")
 
 # Welcome Message
 
@@ -198,3 +144,22 @@ logo = pyfiglet.figlet_format("IP Subnetting")
 print(logo)
 print("Which option would you like to select?\n")
 menu()
+ip_binary = dec2bin(ip)
+subnet_binary = dec2bin(subnet)
+print('\nIP:', ip)
+print('Subnet:', subnet)
+wildcard_binary = find_wildcard(dec2bin(subnet))
+WildCard = convert_decimal(wildcard_binary)
+print('Wildcard:', WildCard)
+networkID = andOP(ip, subnet)
+network_Binary = dec2bin(networkID)
+print('Network ID:', networkID)
+broadcastIP = orOP(networkID, WildCard)
+broadcastIP_binary = dec2bin(broadcastIP)
+print('Broadcast IP:', broadcastIP)
+maxIP = maxiIP(broadcastIP)
+maxIP_binary = dec2bin(maxIP)
+print('Max. IP:', maxIP)
+minIP = miniIP(networkID)
+minIP_binary = dec2bin(networkID)
+print('Min. IP:', minIP)
