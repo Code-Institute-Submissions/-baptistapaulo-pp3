e9c7ce6142c9a0cb701c0bd74fe0890945ff360e
baptistapaulo <paulo.a.baptista@gmail.com>
2022-11-19 02:17:58 +0000

    Add function validity() to validate IP address and subnet mask within acceptable parameters

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import pyfiglet
 import os
 import time
@@ -23,6 +19,9 @@ def dec2bin(integer):
     binary = '.'.join([bin(int(x)+256)[3:] for x in integer.split('.')])
     return binary
 
+# Wildcard Mask
+
+
 def complement(number):
     if number == '0':
         number = '1'
@@ -32,10 +31,13 @@ def complement(number):
         number = '0'
     return number
 
+
 def find_wildcard(binary_subnet):
     binary_list = list(binary_subnet)
-    wildcard = ''.join(complement(binary_list[y]) for y in range(len(binary_list)))
+    wildcard = ''.join(complement(binary_list[y])
+                       for y in range(len(binary_list)))
     return wildcard
+
 
 def convert_decimal(wildcard_Binary):
     binary = {}
@@ -43,6 +45,97 @@ def convert_decimal(wildcard_Binary):
         binary[x] = int(wildcard_Binary.split(".")[x], 2)
     dec = ".".join(str(binary[x]) for x in range(4))
     return dec
+
+# Network ID
+
+
+def andOP(IP1, IP2):
+    ID_list = {}
+    for y in range(4):
+        ID_list[y] = int(IP1.split(".")[y]) & int(IP2.split(".")[y])
+    ID = ".".join(str(ID_list[z]) for z in range(4))
+    return ID
+
+# Broadcast IP
+
+
+def orOP(IP1, IP2):
+    Broadcast_list = {}
+    for z in range(4):
+        Broadcast_list[z] = int(IP1.split(".")[z]) | int(IP2.split(".")[z])
+    broadcast = ".".join(str(Broadcast_list[c]) for c in range(4))
+    return broadcast
+
+# Max IP
+
+
+def maxiIP(brdcstIP):
+    maxIPs = brdcstIP.split(".")
+    if int(brdcstIP.split(".")[3]) - 1 == 0:
+        if int(brdcstIP.split(".")[2]) - 1 == 0:
+            if int(brdcstIP.split(".")[1]) - 1 == 0:
+                maxIPs[0] = int(brdcstIP.split(".")[0]) - 1
+            else:
+                maxIPs[1] = int(brdcstIP.split(".")[1]) - 1
+        else:
+            maxIPs[2] = int(brdcstIP.split(".")[2]) - 1
+    else:
+        maxIPs[3] = int(brdcstIP.split(".")[3]) - 1
+    return ".".join(str(maxIPs[x]) for x in range(4))
+
+# Min IP
+
+
+def miniIP(ntwrkID):
+    miniIPs = ntwrkID.split(".")
+    if int(ntwrkID.split(".")[3]) + 1 == 256:
+        if int(ntwrkID.split(".")[2]) + 1 == 256:
+            if int(ntwrkID.split(".")[1]) + 1 == 256:
+                miniIPs[0] = int(ntwrkID.split(".")[0]) + 1
+                miniIPs[1] = 0
+                miniIPs[2] = 0
+                miniIPs[3] = 0
+            else:
+                miniIPs[1] = int(ntwrkID.split(".")[1]) + 1
+                miniIPs[2] = 0
+                miniIPs[3] = 0
+        else:
+            miniIPs[2] = int(ntwrkID.split(".")[2]) + 1
+            miniIPs[3] = 0
+    else:
+        miniIPs[3] = int(ntwrkID.split(".")[3]) + 1
+    return ".".join(str(miniIPs[x]) for x in range(4))
+
+
+def validity():
+    # Checking IP address validity
+    ip = input("Enter the NETWORK [x.x.x.x]: ")
+
+    while True:
+            # Checking octets
+            a = ip.split('.')
+
+            if (len(a) == 4) and (1 <= int(a[0]) <= 223) and (int(a[0]) != 127) and (int(a[0]) != 169 or int(a[1]) != 254) and (0 <= int(a[1]) <= 255 and 0 <= int(a[2]) <= 255 and 0 <= int(a[3]) <= 255):
+                break
+
+            else:
+                print("\nThe IP address is INVALID! Please retry!\n")
+                continue
+
+    # Checking Subnet Mask validity
+    while True:
+            subnet = input("Enter the MASK [y.y.y.y]: ")
+            # Checking octets
+            b = subnet.split('.')
+
+            if (len(b) == 4) and (int(b[0]) == 255) and (int(b[1]) in masks) and (int(b[2]) in masks) and (int(b[3]) in masks) and (int(b[0]) >= int(b[1]) >= int(b[2]) >= int(b[3])):
+              
+                break
+
+            else:
+                print("\nThe subnet mask is INVALID! Please retry!\n")
+                continue
+
 
 def menu():
     """
@@ -68,8 +161,6 @@ def menu():
             break
 
 
-
-
 # Welcome Message
 logo = pyfiglet.figlet_format("IP Subnetting")
 print(logo)
@@ -86,8 +177,7 @@ cls_terminal()
 logo = pyfiglet.figlet_format("IP Subnetting")
 print(logo)
 print("Provide the following details.\n")
-ip = input("Enter the NETWORK [x.x.x.x]: ")
-subnet = input("Enter the MASK [y.y.y.y]: ")
+validity()
 input("\nPress Enter to continue...")
 cls_terminal()
 # Option's Message
@@ -102,3 +192,15 @@ print('Subnet:', Subnet, "->", subnet_binary)
 wildcard_binary = find_wildcard(dec2bin(Subnet))
 WildCard = convert_decimal(wildcard_binary)
 print('Wildcard:', WildCard, '->', wildcard_binary)
+networkID = andOP(IP, Subnet)
+network_Binary = Int2Bin(networkID)
+print('Network ID:', networkID, "->", network_Binary)
+broadcastIP = orOP(networkID, WildCard)
+broadcastIP_binary = Int2Bin(broadcastIP)
+print('Broadcast IP:', broadcastIP, "->", broadcastIP_binary)
+maxIP = maxiIP(broadcastIP)
+maxIP_binary = Int2Bin(maxIP)
+print('Max. IP:', maxIP, "->", maxIP_binary)
+minIP = miniIP(networkID)
+minIP_binary = Int2Bin(networkID)
+print('Min. IP:', minIP, "->", minIP_binary)
